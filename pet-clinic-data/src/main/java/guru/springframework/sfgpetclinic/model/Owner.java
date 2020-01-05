2e5e5f23bd727b00e421839ceb586cb64a90d249
alpergng <gungor.alper@yandex.com>
2020-01-05 23:50:58 +0300

    added visits, updated owner with pet relationship. closes #26

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Ag on 19.7.2019
  */
 public class Owner extends Person{
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -0,0 +1,37 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ * Created by Ag on 5.1.2020
+ */
+public class Visit extends BaseEntity {
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
