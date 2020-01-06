c0277520a40b9c555d930779320815ed28c32501
alpergng <gungor.alper@yandex.com>
2020-01-06 22:10:12 +0300

    Added Specialities Map Impl , Closes #31.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+/**
+ * Created by Ag on 6.1.2020
+ */
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
